0568e625d313acb9266a6f39eb2679577ea3571b
TomsonOO <tomasz.rzepka52gmail.com>
2023-12-26 15:14:23 +0100

    added more tests

--- multiply.py
+++ multiply.py
@@ -11,6 +11,19 @@ class MultiplyTestCase(unittest.TestCase):
         self.assertEqual(multiply(-1, 2), -2)
         self.assertEqual(multiply(0, 10), 0)
 
+class TestAdditionalCases(unittest.TestCase):
+    def test_multiply_identity(self):
+        self.assertEqual(multiply(1, 5), 5)
+        self.assertEqual(multiply(1, -5), -5)
+
+    def test_multiply_zero(self):
+        self.assertEqual(multiply(0, 5), 0)
+        self.assertEqual(multiply(0, -5), 0)
+
+    def test_multiply_negatives(self):
+        self.assertEqual(multiply(-1, -5), 5)
+        self.assertEqual(multiply(-3, -3), 9)
+
 # This makes the test cases run when the file is executed
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
